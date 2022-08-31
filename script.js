e67ae242769f69c893931d4ea174f3cb2374b6ed
Ogbondah Jessica <ogbondahj@gmail.com>
2022-08-31 23:49:05 +0100

    update

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
+//Activating Strict Mode
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
 
-// const interface = Audio;
-// const private = 534;
+const interface = 'Audio';
+const private = 534;
+
+// //FUNCTIONS
+function logger() {
+	console.log('My name is Jessica');
+}
+
+//calling / running / invoking the function refers to when a function is being reused.
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+	return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+
+//FUNCTION DECLARATIONS VS EXPRESSIONS
+// Function declaration
+function calcAge1(birthYear) {
+	return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+//Function Expression
+const calcAge2 = function (birthYear) {
+	return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+*/
+//ARROW FUNCTIONS
+//Function Expression
+const calcAge2 = function (birthYear) {
+	return 2037 - birthYear;
+};
+
+//Arrow Function
+const calcAge3 = (birthYear) => 2037 - birthYear;
