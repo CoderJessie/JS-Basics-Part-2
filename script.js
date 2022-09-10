b30c69ab808243dca7c3ed7d140d79fc11ad20bc
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-10 21:57:52 +0100

    code challenge #3 solution

--- script.js
+++ script.js
@@ -427,4 +427,55 @@ console.log(jonas.age);
 // "Jonas is a 46-year old teacher, and he has a driver's license"
 console.log(jonas.getSummary());
 */
+////////////////////////////////////
 ///Coding Challenge #3
+/*
+Let's go back to Mark and John comparing their BMI's! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and 1.95 m tall.
+
+GOOD LUCK
+*/
+
+////SOLUTION
+const mark = {
+	firstName: 'Mark',
+	lastName: 'Miller',
+	fullName: 'Mark Miller',
+	mass: 78,
+	height: 1.69,
+	calcBMI: function (BMI) {
+		this.BMI = this.mass / this.height ** 2;
+		return this.BMI;
+	},
+	getSummary: function () {
+		return `${this.fullName}'s BMI ${this.BMI} is ${
+			this.BMI > john.BMI ? 'higher' : 'not higher'
+		} than ${john.fullName}'s BMI ${john.BMI}!`;
+	},
+};
+
+const john = {
+	firstName: 'John',
+	lastName: 'Smith',
+	fullName: 'John Smith',
+	mass: 92,
+	height: 1.95,
+	calcBMI: function (BMI) {
+		this.BMI = this.mass / this.height ** 2;
+		return this.BMI;
+	},
+	getSummary: function () {
+		return `${this.fullName}'s BMI ${this.BMI} is ${
+			this.BMI > mark.BMI ? 'higher' : 'not higher'
+		} than ${mark.fullName}'s ${mark.BMI}!`;
+	},
+};
+console.log(mark.calcBMI());
+console.log(john.calcBMI());
+console.log(mark.getSummary());
+console.log(john.getSummary());
