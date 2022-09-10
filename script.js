441ca9828250c535a0a4d624547bc16febb2c116
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-10 11:57:36 +0100

    challenge on object methods

--- script.js
+++ script.js
@@ -400,12 +400,29 @@ const jonas = {
 	// 	return 2037 - birthYear;
 	// },
 
-	calcAge: function(){
-		console.log(this); //'this keyword points to the object 'jonas'
-		return 2037 - this.birthYear;
-	}
+	// calcAge: function(){
+	// 	// console.log(this); //'this keyword points to the object 'jonas'
+	// 	return 2037 - this.birthYear;
+	// }
+
+	calcAge: function () {
+		this.age = 2037 - this.birthYear;
+		return this.age;
+	},
+
+	getSummary: function () {
+		return `${this.firstName} is a ${this.age}-year old ${
+			this.job
+		}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`;
+	},
 };
 
 console.log(jonas.calcAge());
 
-// console.log(jonas[`calcAge`](1991));
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+
+//Challenge
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+console.log(jonas.getSummary());
