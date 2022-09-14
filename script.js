e3959cdffb9d097892d5235d70376819f5321464
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-14 10:25:53 +0100

    code challenge #4 bonus

--- script.js
+++ script.js
@@ -613,3 +613,17 @@ for (let i = 0; i < bills.length; i++) {
 	totals.push(bills[i] + tips[i]);
 }
 console.log(bills, tips, totals);
+
+////BONUS SOUTION
+const calcAverage = function (arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		// sum = sum + arr[i];
+		sum += arr[i];
+	}
+	console.log(sum);
+	return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
