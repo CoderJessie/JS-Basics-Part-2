105d605be42ac1eb5a8b4d7196c1953e0e276255
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-22 00:29:31 +0100

    project 1

--- Guess My Number_Project 1/script.js
+++ Guess My Number_Project 1/script.js
@@ -1,3 +1,6 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent); //since its a class use(.) but if id use(#)
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
