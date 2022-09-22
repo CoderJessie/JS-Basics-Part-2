c4dec96336cb6783e3d08e4973666528171160b0
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-22 18:10:35 +0100

    project 1

--- Guess My Number_Project 1/script.js
+++ Guess My Number_Project 1/script.js
@@ -2,5 +2,9 @@
 
 console.log(document.querySelector('.message').textContent); //since its a class use(.) but if id use(#)
 document.querySelector('.message').textContent = 'Correct Number!';
+
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
