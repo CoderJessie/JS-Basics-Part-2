d98c822cd647e782671b06be4976a17d0836df9f
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-23 03:05:26 +0100

    Handling click events

--- Guess My Number_Project 1/script.js
+++ Guess My Number_Project 1/script.js
@@ -14,5 +14,10 @@ console.log(document.querySelector('.guess').value);
 */
 //Handling Click Events
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
 });
