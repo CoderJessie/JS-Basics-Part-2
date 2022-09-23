e691aedce0207737ef69dd0ebfac0a7d309a87cd
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-23 02:48:21 +0100

    Project 1

--- Guess My Number_Project 1/script.js
+++ Guess My Number_Project 1/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent); //since its a class use(.) but if id use(#)
+
+//Selecting and Manipulatig Elements
 document.querySelector('.message').textContent = 'Correct Number!';
 
 document.querySelector('.number').textContent = 13;
@@ -8,3 +11,8 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+//Handling Click Events
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
