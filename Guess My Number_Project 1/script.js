fbd72acb090f9fbc98ffc5ab47fe7d59bbb0f1c0
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-25 22:24:50 +0100

    code challenge #1

--- Guess My Number_Project 1/script.js
+++ Guess My Number_Project 1/script.js
@@ -13,7 +13,7 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
 //Handling Click Events
@@ -56,3 +56,16 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+//////////////////////////////////
+// Coding Challenge #1
+/*
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+GOOD LUCK!
+*/
