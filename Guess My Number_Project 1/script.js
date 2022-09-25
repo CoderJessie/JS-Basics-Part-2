d77ac9d93cf08ec3460ac98405d4ed4f182fb76d
Ogbondah Jessica <ogbondahj@gmail.com>
2022-09-25 20:51:39 +0100

    project 1

--- Guess My Number_Project 1/script.js
+++ Guess My Number_Project 1/script.js
@@ -15,7 +15,7 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 //Handling Click Events
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,9 +28,9 @@ document.querySelector('.check').addEventListener('click', function () {
     //when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
 
     //When guess is too high
@@ -47,7 +47,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
+      document.querySelector('.message').textContent = 'Too low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
