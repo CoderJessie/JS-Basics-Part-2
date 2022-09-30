3f668097eabf1c64393541d67604b1b96fca0b6a
Ogbondah Jessica <ogbondahj@gmail.com>
2022-10-01 00:06:49 +0100

    removing classes

--- Modal_Project 2/script.js
+++ Modal_Project 2/script.js
@@ -4,11 +4,15 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const openModal = function () {
+	console.log('Button clicked');
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-	console.log(
-		btnsOpenModal[i].addEventListener('click', function () {
-			console.log('Button clicked');
-		})
-	);
+	btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
