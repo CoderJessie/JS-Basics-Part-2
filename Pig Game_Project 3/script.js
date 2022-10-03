633a3f58a5444e7728f35e28d7a49e7b0318aff5
Ogbondah Jessica <ogbondahj@gmail.com>
2022-10-03 09:37:44 +0100

    project 3

--- Pig Game_Project 3/script.js
+++ Pig Game_Project 3/script.js
@@ -4,5 +4,8 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 
+//Resetting the scores
 score0El.textContent = 0;
 score1El.textContent = 0;
+
+//Hidding the dice
