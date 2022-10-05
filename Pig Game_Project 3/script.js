992589e02fd917c5d82ebcc1506b26fda3a38669
Ogbondah Jessica <ogbondahj@gmail.com>
2022-10-05 16:17:13 +0100

    project 3

--- Pig Game_Project 3/script.js
+++ Pig Game_Project 3/script.js
@@ -44,6 +44,6 @@ btnRoll.addEventListener('click', function () {
 		currentScore = 0;
 		activePlayer = activePlayer === 0 ? 1 : 0;
 		player0El.classList.toggle('player--active');
-		playerEl.classList.toggle('player--active');
+		player1El.classList.toggle('player--active');
 	}
 });
